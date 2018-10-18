367c884ff73e925784306fd4d68bf905942dc7b5
sillyvester <jsylvester@smu.edu>
2018-10-18 11:34:48 -0500

    made navbar shared component

--- personal-finance/src/app/app.module.ts
+++ personal-finance/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
