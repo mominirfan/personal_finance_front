9b2c5e3b217332b523c211c4e3fbc915a19edf31
mominirfan <momin.irfan@live.com>
2018-10-18 15:27:15 -0500

    Adding chart package

--- personal-finance/src/app/app.module.ts
+++ personal-finance/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { ChartComponent } from './chart/chart.component';
+import { PiechartDirective } from './piechart.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    NavBarComponent
+    NavBarComponent,
+    ChartComponent,
+    PiechartDirective
   ],
   imports: [
     BrowserModule
