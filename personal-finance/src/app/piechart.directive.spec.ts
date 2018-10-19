5fe259596fbbd535bb19d335cb201f7778e42b27
mominirfan <momin.irfan@live.com>
2018-10-18 19:21:08 -0500

    adding chart again

---
+++ personal-finance/src/app/piechart.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PiechartDirective } from './piechart.directive';
+
+describe('PiechartDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PiechartDirective();
+    expect(directive).toBeTruthy();
+  });
+});
