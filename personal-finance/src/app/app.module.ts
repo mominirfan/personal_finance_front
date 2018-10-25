d09394fa4133154091daad30e77cec787fe704f3
kr0wl <fcorral@smu.edu>
2018-10-25 11:04:40 -0500

    shrug

--- personal-finance/src/app/app.module.ts
+++ personal-finance/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { ChartComponent } from './chart/chart.component';
 import { PiechartDirective } from './piechart.directive';
-import{ LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component';
 import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
