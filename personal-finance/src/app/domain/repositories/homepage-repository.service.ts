37b7bf0776eda953a9ef54276927824d7d6a34ac
sillyvester <jsylvester@smu.edu>
2018-11-01 19:46:36 -0500

    fixed domain folder

---
+++ personal-finance/src/app/domain/repositories/homepage-repository.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// @Injectable()
+// export class HomepageRepository {
+
+//   protected endpoint = 'http://ec2-3-16-30-192.us-east-2.compute.amazonaws.com:8080/api/registration';
+
+//   constructor (private httpClient: HttpClient) {}
+
+//   public register(username: Account): Observable<Account> {
+//     return this.httpClient.post(this.endpoint, account);
+//   }
+// }
