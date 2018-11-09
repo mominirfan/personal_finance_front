99d9871986aefb3d4fe485bd155095cb107fcb5c
sillyvester <jsylvester@smu.edu>
2018-11-08 18:44:25 -0600

    Made all things on dashboard populate from DB

--- personal-finance/src/app/domain/repositories/homepage-repository.service.ts
+++ personal-finance/src/app/domain/repositories/homepage-repository.service.ts
@@ -1,14 +1,25 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// @Injectable()
-// export class HomepageRepository {
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Bill } from '../models/bill';
+import { Suggestion } from '../models/suggestion';
 
-//   protected endpoint = 'http://ec2-3-16-30-192.us-east-2.compute.amazonaws.com:8080/api/registration';
+@Injectable({
+  providedIn: 'root'
+})
+export class HomepageRepository {
 
-//   constructor (private httpClient: HttpClient) {}
+  protected endpoint = 'http://ec2-3-16-30-192.us-east-2.compute.amazonaws.com:8080/api/get-budgets/postMalone';
+  protected suggestionEndpoint = 'http://ec2-3-16-30-192.us-east-2.compute.amazonaws.com:8080/api/get-suggs/';
 
-//   public register(username: Account): Observable<Account> {
-//     return this.httpClient.post(this.endpoint, account);
-//   }
-// }
+  constructor (private httpClient: HttpClient) {}
+
+  getBills(): Observable<Bill[]> {
+    return this.httpClient.get<Bill[]>(this.endpoint);
+  }
+
+  getSuggestions(type: string): Observable<Suggestion[]> {
+    return this.httpClient.get<Suggestion[]>(`${this.suggestionEndpoint}${type}`);
+  }
+
+}
