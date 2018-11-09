ad2c49b69198b4425c23846dfa4fee3a47dadc1f
sillyvester <jsylvester@smu.edu>
2018-11-08 19:44:17 -0600

    added loans to navbar and made it it's own page

--- personal-finance/src/app/app-routing.module.ts
+++ personal-finance/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoansComponent } from './loans/loans.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,7 +13,8 @@ const appRoutes: Routes = [
   {path: 'register', component: RegistrationComponent},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'budget', component: BudgetComponent},
-  { path: 'budget-edit', component: BudgetEditorComponent}
+  { path: 'budget-edit', component: BudgetEditorComponent},
+  { path: 'loans', component: LoansComponent}
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(appRoutes) ],
--- personal-finance/src/app/loans/loans.component.ts
+++ personal-finance/src/app/loans/loans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loan } from '../domain/models/loan';
 
 @Component({
   selector: 'app-loans',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoansComponent implements OnInit {
 
+  loans: Loan;
   constructor() { }
 
   ngOnInit() {
