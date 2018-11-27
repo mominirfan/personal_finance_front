c3e01b7f012d21201783f92cd95eba8af110357c
1genericusername <alarellano@smu.edu>
2018-11-27 17:23:51 -0600

    seems to work

---
+++ personal-finance/src/app/domain/repositories/settings-repository.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsRepository {
+
+  protected endpoint = 'http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/';
+
+  constructor (private httpClient: HttpClient) {}
+
+  updatePassword(userName: String, Pword: String) {
+    return this.httpClient.put<any>(`${this.endpoint}edit-pass`, {userName: userName, Pword: Pword});
+  }
+
+  updateIncome(userName: String, income: number) {
+    return this.httpClient.put<any>(`${this.endpoint}edit-inc`, {userName: userName, income: income});
+  }
+
+  updateBalance(userName: String, balance: number) {
+    return this.httpClient.put<any>(`${this.endpoint}edit_bal`, {userName: userName, bal: balance});
+  }
+}
--- personal-finance/src/app/settings/settings.component.ts
+++ personal-finance/src/app/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { SettingsRepository } from '../domain/repositories/settings-repository.service';
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -7,23 +9,37 @@ import { FormGroup } from '@angular/forms';
 })
 export class SettingsComponent implements OnInit {
   currentUser: any = { };
-  curPass: string;
   newPass: string;
   newPass2: string;
-  curPass2: string;
-
+  newBalance: number;
   newIncome: number;
-  constructor() { }
+  constructor(private sr: SettingsRepository) { }
 
   ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
   submitPR( passwordResetForm: FormGroup) {
-    console.log("Submitting Password Reset Form");
+    console.log('Submitting Password Reset Form');
+    if (this.newPass === this.newPass2) {
+      this.sr.updatePassword(this.currentUser.userName, this.newPass).subscribe(x => this.onUpdate(x));
+    }
   }
 
   submitI( incomeUpdateFrom: FormGroup) {
-    console.log("Submitting Income Update Form");
+    console.log('Submitting Income Update Form');
+    this.sr.updateIncome(this.currentUser.userName, this.newIncome).subscribe(x => this.onUpdate(x));
+  }
+
+  submitB( incomeUpdateFrom: FormGroup) {
+    console.log('Submitting Balance Update Form');
+    this.sr.updateBalance(this.currentUser.userName, this.newBalance).subscribe(x => this.onUpdate(x));
+  }
+
+  onUpdate(a: any) {
+    this.newPass = null;
+    this.newPass2 = null;
+    this.newIncome = null;
+    this.newBalance = null;
   }
 }
