ad373bcc72d0df770662e5af38b8d5f8bd63e625
sillyvester <jsylvester@smu.edu>
2018-12-02 17:48:19 -0600

    cool

--- personal-finance/src/app/chart/chart.component.ts
+++ personal-finance/src/app/chart/chart.component.ts
@@ -1,61 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import * as CanvasJS from './canvasjs.min';
-import { HomepageRepository } from '../domain/repositories/homepage-repository.service';
-import { Bill } from '../domain/models/bill';
-
+import { Component, OnInit } from "@angular/core";
+import * as CanvasJS from "./canvasjs.min";
+import { HomepageRepository } from "../domain/repositories/homepage-repository.service";
+import { Bill } from "../domain/models/bill";
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  selector: "app-chart",
+  templateUrl: "./chart.component.html",
+  styleUrls: ["./chart.component.css"]
 })
 export class ChartComponent implements OnInit {
-
   budget_labels = [];
   spend_vals = [];
   bills: Bill[];
   currentUser: any = {};
-  constructor(
-    private homepageRepo: HomepageRepository,
-  ) {
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  constructor(private homepageRepo: HomepageRepository) {
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
   }
 
   ngOnInit() {
-
-    this.homepageRepo.getBills(this.currentUser.userName).subscribe((bills) => {
+    this.homepageRepo.getBills(this.currentUser.userName).subscribe(bills => {
       this.bills = bills;
       for (const bill of this.bills) {
         this.budget_labels.push(bill.budgetType);
         const amt = +bill.amt;
         this.spend_vals.push(amt);
-     }
+      }
 
+      let spend_points = [];
 
-     let spend_points = [];
+      let i;
+      for (i = 0; i < this.budget_labels.length; i++) {
+        spend_points.push({
+          y: this.spend_vals[i],
+          name: this.budget_labels[i]
+        });
+      }
 
-     let i;
-     for (i = 0 ; i < this.budget_labels.length ; i++) {
-       spend_points.push({y: this.spend_vals[i], name: this.budget_labels[i]}) ;
-     }
+      let chart = new CanvasJS.Chart("chartContainer", {
+        theme: "light2",
+        animationEnabled: true,
+        exportEnabled: true,
+        data: [
+          {
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+            indexLabel: "{name} - #percent%",
+            dataPoints: spend_points
+          }
+        ]
+      });
 
-
-     let chart = new CanvasJS.Chart('chartContainer', {
-       theme: 'light2',
-       animationEnabled: true,
-       exportEnabled: true,
-       data: [{
-         type: 'pie',
-         showInLegend: true,
-         toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
-         indexLabel: '{name} - #percent%',
-         dataPoints: spend_points
-       }]
-     });
-
-     chart.render();
+      chart.render();
     });
-
-
   }
 }
--- personal-finance/src/app/domain/repositories/expenses.services.ts
+++ personal-finance/src/app/domain/repositories/expenses.services.ts
@@ -1,4 +1,4 @@
-import { Expense } from './../models/expense';
+import { Expense } from "./../models/expense";
 import { Injectable } from "@angular/core";
 import { HttpHeaders, HttpClient } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
@@ -6,41 +6,39 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class ExpensesService {
+  protected getAllExpensesEndpoint =
+    "http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/get-expenses";
+  protected addExpenseEndpoint =
+    "http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/add-expense";
+  protected getSummedExpenses =
+    "http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/get-total-spending";
 
-  protected getAllExpensesEndpoint = 'http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/get-expenses';
-  protected addExpenseEndpoint = 'http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/add-expense';
-  protected getSummedExpenses = 'http://ec2-18-224-109-208.us-east-2.compute.amazonaws.com:8080/api/get-total-spending';
 
-  // protected httpOptions = {
-  //   headers: new HttpHeaders({
-  //     'Content-Type': 'application/json',
-  //     'Authorization': 'johnlawrimore'
-  //   })
-  // };
-
-  constructor( protected httpClient: HttpClient ) {
-  }
+  constructor(protected httpClient: HttpClient) {}
 
   public getExpenses(username: String) {
-    return this.httpClient.get<[{}]>(`${this.getAllExpensesEndpoint}/${username}`)
-    .pipe(catchError(this.handleException));
+    return this.httpClient
+      .get<[{}]>(`${this.getAllExpensesEndpoint}/${username}`)
+      .pipe(catchError(this.handleException));
   }
 
   public getExpenseSum(username: String) {
-    return this.httpClient.get<[{}]>(`${this.getSummedExpenses}/${username}`)
-    .pipe(catchError(this.handleException));
+    return this.httpClient
+      .get<[{}]>(`${this.getSummedExpenses}/${username}`)
+      .pipe(catchError(this.handleException));
   }
 
   public addExpense(ex: Expense) {
-    return this.httpClient.post<[{}]>(`${this.addExpenseEndpoint}`, ex)
-    .pipe(catchError(this.handleException));
+    return this.httpClient
+      .post<[{}]>(`${this.addExpenseEndpoint}`, ex)
+      .pipe(catchError(this.handleException));
   }
 
   protected handleException(exception: any) {
-    const message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
+    const message = `${exception.status} : ${exception.statusText}\r\n${
+      exception.message
+    }`;
     alert(message);
     return Observable.throw(exception);
   }
-
-
 }
